c525875df7ae1b5598795cbd5b870a8f89da7663
kclim1 <kclim0528@gmail.com>
2025-10-06 18:50:13 +0800

    created CRUD logic for product's service layer

---
+++ src/main/java/com/example/productbackend/service/ProductService.java
@@ -0,0 +1,41 @@
+package com.example.productbackend.service;
+
+import com.example.productbackend.dto.NewProductRequestDTO;
+import com.example.productbackend.dto.ProductResponseDTO;
+import com.example.productbackend.product.Product;
+import com.example.productbackend.repository.ProductRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<ProductResponseDTO> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        return products.stream().map(product -> new ProductResponseDTO("", product)).toList();
+    }
+
+    public ProductResponseDTO fetchProductById(UUID id) {
+        Product product = productRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "product with id not found"));
+        return new ProductResponseDTO("item found",product);
+    }
+
+    public void deleteProductById(UUID id) {
+        productRepository.deleteById(id);
+    }
+
+    public Product addProduct(NewProductRequestDTO dto) {
+        Product newProduct = new Product(dto.name(), dto.description(), dto.price(), dto.imageUrl(), dto.stockLevel());
+        productRepository.save(newProduct);
+        return newProduct;
+    }
+}
