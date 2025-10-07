4436b39d7b7167490ea48872d750df78a4b35494
kclim1 <kclim0528@gmail.com>
2025-10-07 21:59:02 +0800

    added bean validations to the Request DTO

--- src/main/java/com/example/productbackend/dto/NewProductRequestDTO.java
+++ src/main/java/com/example/productbackend/dto/NewProductRequestDTO.java
@@ -1,5 +1,9 @@
 package com.example.productbackend.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+
 import java.math.BigDecimal;
 
-public record NewProductRequestDTO(String name, String description, BigDecimal price, String imageUrl , int stockLevel) {}
+public record NewProductRequestDTO(@NotBlank String name, String description, @Positive BigDecimal price, String imageUrl ,@PositiveOrZero int stockLevel) {}
